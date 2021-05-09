84a5be78921f156700061d502b369910fbc440d3
dmitry-mingazov <mingazov@email.com>
2021-05-09 10:38:28 +0200

    Add sidebar with Action Buttons

--- src/main/java/com/sample/gui/javafx/JavaFXApplication.java
+++ src/main/java/com/sample/gui/javafx/JavaFXApplication.java
@@ -1,11 +1,19 @@
 package com.sample.gui.javafx;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sample.Moves;
+
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Separator;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -26,6 +34,10 @@ public class JavaFXApplication extends Application {
     double width;
     double height;
     
+    private double consoleHeight = 100;
+    private double sidebarWidth = 200;
+    private double buttonWidth = 100;
+    
     private String imageURL = "https://opengameart.org/sites/default/files/tileable_grass.png";
 
     @Override
@@ -35,7 +47,7 @@ public class JavaFXApplication extends Application {
     	this.columns = conf.getColumns();
     	this.width = conf.getWidth();
     	this.height = conf.getHeight();
-
+    	
     	try {
             StackPane root = new StackPane();
 			ImageView imageView = new ImageView( new Image(this.imageURL, this.width, this.height, false, false));
@@ -58,12 +70,24 @@ public class JavaFXApplication extends Application {
            
             TextArea textarea = new TextArea();
             textarea.setStyle("-fx-font-size: 2em;");
-            textarea.setPrefHeight(100);
+            textarea.setPrefHeight(this.consoleHeight);
+            textarea.setMaxWidth(this.width);
+            
+            Separator sep1 = new Separator();
+            Separator sep2 = new Separator();
 
             VBox vbox = new VBox(root, textarea);
-
+            
+            VBox buttons = new VBox();
+            buttons.setSpacing(10);
+//            Button moveUp = new Button();
+//            moveUp.setPrefWidth(100);
+            buttons.setPadding(new Insets(0, 20, 10, 20));
+            buttons.getChildren().addAll(this.getSidebarButtons());
+            HBox hbox = new HBox(vbox, sep1, buttons, sep2);
+            
             // create scene and stage
-            Scene scene = new Scene(vbox, width, height+100);
+            Scene scene = new Scene(hbox, width + this.sidebarWidth, height + this.consoleHeight);
             scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
             primaryStage.setScene(scene);
             primaryStage.show();
@@ -74,6 +98,34 @@ public class JavaFXApplication extends Application {
             e.printStackTrace();
         }
     }
+    
+    private List<ActionButton> getSidebarButtons() {
+    	List<ActionButton> actionButtons = new ArrayList<ActionButton>();
+    	
+    	for(Moves move : Moves.values()) {
+    		actionButtons.add(new ActionButton(this.buttonWidth, move));
+    	}
+    	
+    	return actionButtons;
+//    	return (ActionButton[])actionButtons.toArray();
+
+    }
+    
+    private class ActionButton extends Button {
+    	private Moves move;
+    	public ActionButton(double width, Moves move) {
+    		this.move = move;
+    		this.setText(move.toString());
+    		this.maxWidth(Double.MAX_VALUE);
+//    		this.prefWidth(width);
+//    		this.minWidth(width);
+    	}
+    	
+    	public Moves getMove() {
+    		return this.move;
+    	}
+    }
+    
 
     public static void main(String[] args) {
     	AppConfiguration conf = AppConfiguration.getInstance();
